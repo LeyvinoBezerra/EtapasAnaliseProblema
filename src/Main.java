11e51d30e0fd38be3858f603897d008f6708f643
Leyva Bezerra <47372809+leyvinobezerra@users.noreply.github.com>
2025-10-11 18:28:24 -0300

    Add

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/* Analise critica do problema e descubra:
+(Tente explicar este problema para você mesmo em voz alta e peça mais
+informações/investigue mais até você compreeder completamente o problema.)
+
+1. Quais são os dados de entrada necessarios?
+
+2. oque devo fazer com esses dados ?
+
+3. Quais as restriçoes deste problema?
+
+4. Qual é o rersultado esperado?
+
+5.Qual é a seguencia de passos a ser feita para chegar ao resultado esperado?
+ */
